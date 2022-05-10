79d9ae012eb1176828287df5e18cd389887bce20
Sangram <sangram15.behera@gmail.com>
2022-05-11 02:12:28 +0530

    Added By Sangram

---
+++ EmployeeWage/UC3.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmployeeWageComputation
+{
+
+    class UC3
+    {
+        public void partTime()
+        {
+
+            int empwageperhrs = 20;
+            int emphrs;
+            Random CheckempAttandance = new Random();
+            int ispresent = CheckempAttandance.Next(0, 3);
+            Console.WriteLine(ispresent);
+            if (ispresent == 1)
+            {
+                Console.WriteLine("Employee is present!");
+                Console.WriteLine("Employee is Full Time");
+                emphrs = 8;
+
+            }
+            else if (ispresent == 2)
+            {
+                Console.WriteLine("Employee is Present!");
+                Console.WriteLine("Employee is Part Time");
+                emphrs = 4;
+            }
+
+            else
+            {
+                Console.WriteLine("Employee is Absent!");
+                emphrs = 0;
+
+            }
+            int Salary = emphrs * empwageperhrs;
+            Console.WriteLine("Employee Salary = " + Salary);
+        }
+    }
+}
